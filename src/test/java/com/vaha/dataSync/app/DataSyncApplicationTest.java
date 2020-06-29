f6b84b7c1555f72c80cdc4f980cc066f20ccfa3e
momoKr <playmomo71@gmail.com>
2020-06-29 19:19:12 +0900

    컴파일 오류 수정(maven bulid 에서 profiles항목에 pom.xml 삭제)

---
+++ src/test/java/com/vaha/dataSync/app/DataSyncApplicationTest.java
@@ -0,0 +1,13 @@
+package com.vaha.dataSync.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSyncApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
--- src/test/java/com/vaha/dataSync/app/web/WelcomeTestController.java
+++ src/test/java/com/vaha/dataSync/app/web/WelcomeTestController.java
@@ -1,14 +1,12 @@
 package com.vaha.dataSync.app.web;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class WelcomeTestController {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+
 	}
 
 }
